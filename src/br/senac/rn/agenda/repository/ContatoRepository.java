bee6d15b3ff6bb5d4666c61d3f49d2f0a663c8a3
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-08-12 20:21:17 -0300

    criando o list e arraylist

--- src/br/senac/rn/agenda/repository/ContatoRepository.java
+++ src/br/senac/rn/agenda/repository/ContatoRepository.java
@@ -1,4 +1,14 @@
 package br.senac.rn.agenda.repository;
 
+import br.senac.rn.agenda.model.Contato;
+
+import java.util.ArrayList;
+import java.util.List; // é um collection
+
 public class ContatoRepository {
+
+    private static List<Contato> contatos = new ArrayList<>();      //static pode mudar;
+
+
+
 }
