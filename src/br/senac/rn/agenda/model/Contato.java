45edf9cb150a03114cde15ced43f94b31eb87535
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-08-12 19:31:45 -0300

    criando os getter and setter

--- src/br/senac/rn/agenda/model/Contato.java
+++ src/br/senac/rn/agenda/model/Contato.java
@@ -8,5 +8,31 @@ public class Contato {
     //controller  - regra de negocios;
     //repository ou DAO - é aonde ficam os dados persistentes, vulgo, banco de dados;
 
+    private Long id;
+    private String nome;
+    private String fone;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
 }
