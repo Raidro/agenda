9c29744a8ebc88b77b0b32759e0617e4b95d7665
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-08-12 19:13:48 -0300

    comentarios

--- src/br/senac/rn/agenda/model/Contato.java
+++ src/br/senac/rn/agenda/model/Contato.java
@@ -1,4 +1,12 @@
 package br.senac.rn.agenda.model;
 
 public class Contato {
+
+    //definir o que as coisas são dentro do seu sistema;
+    //model  - define o conceito das coisas dentro do sistema, como serão representados;
+    //view - é a parte visual o que tem contato com o usuario (html, css, js);
+    //controller  - regra de negocios;
+    //repository ou DAO - é aonde ficam os dados persistentes, vulgo, banco de dados;
+
+
 }
