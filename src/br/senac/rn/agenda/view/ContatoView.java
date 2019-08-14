e6b3731f321d9c7c626f0e43a4cf708c3728b342
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-08-14 18:47:27 -0300

    criado as opção no view

--- src/br/senac/rn/agenda/view/ContatoView.java
+++ src/br/senac/rn/agenda/view/ContatoView.java
@@ -1,4 +1,17 @@
 package br.senac.rn.agenda.view;
 
 public class ContatoView {
+
+    public static void main(String[] args) {
+
+        String menu = "";
+
+        menu += "1 - Cadastrar Novo Contato \n";
+        menu += "2 - Listar todos os Contatos \n";
+        menu += "3 - Buscar Contato \n";
+        menu += " ============================ ";
+        menu += "Opção: ";
+
+    }
+
 }
