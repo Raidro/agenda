c7bc16c1f11e344bcfe160f7ae78482728a17e20
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-08-14 18:54:16 -0300

    criado um method de menu

--- src/br/senac/rn/agenda/view/ContatoView.java
+++ src/br/senac/rn/agenda/view/ContatoView.java
@@ -4,14 +4,21 @@ public class ContatoView {
 
     public static void main(String[] args) {
 
+
+
+        System.out.println(getMenu());
+
+    }
+
+    private static String getMenu() {
         String menu = "";
-
-        menu += "1 - Cadastrar Novo Contato \n";
-        menu += "2 - Listar todos os Contatos \n";
-        menu += "3 - Buscar Contato \n";
-        menu += " ============================ ";
+        menu += " ============================  \n";
+        menu += "1 - Cadastrar Novo Contato     \n";
+        menu += "2 - Listar todos os Contatos   \n";
+        menu += "3 - Buscar Contato             \n";
+        menu += " ============================  \n";
         menu += "Opção: ";
-
+        return menu;
     }
 
 }
