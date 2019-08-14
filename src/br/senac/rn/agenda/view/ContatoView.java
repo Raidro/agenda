615e0cacee3c051f21fc8070d6f6922989025e2d
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-08-14 20:31:55 -0300

    criado os metohos de buscarTodos e Cadastrar

--- src/br/senac/rn/agenda/view/ContatoView.java
+++ src/br/senac/rn/agenda/view/ContatoView.java
@@ -8,43 +8,48 @@ import java.util.Scanner;
 public class ContatoView {
 
     private static Scanner entrada = new Scanner(System.in);
-
+    private static ContatoRepository bancoDeDados = new ContatoRepository();
 
     public static void main(String[] args) {
 
         Long escolha = 1L;
-        ContatoRepository bancoDeDados = new ContatoRepository();
+
 
         do {
             ContatoView.getMenu();
             escolha = entrada.nextLong();
 
-            if (escolha == 1) {
 
-                String nome;
-                String fone;
-                System.out.print("Digite o nome do contato: ");
-                nome = entrada.next();
-                System.out.print("Digite o telefone do contato: ");
-                fone = entrada.next();
-
-                Contato contato = new Contato(nome, fone);
-                bancoDeDados.salva(contato);
-                System.out.println("\n" + "Contato salvo com Sucesso!!");
-                System.out.println("Contato Salvo: " + contato.getNome() + "\n" + "Telefone: " + contato.getFone() + "\n");
-//                System.out.println("Contato: " + bancoDeDados.buscaTodos());
+            switch (Integer.parseInt(escolha.toString())) {
+                case 1:
+                    ContatoView.cadastrar();
+                    break;
+                case 2:
+                    ContatoView.listarTodos();
+                    break;
             }
 
-            if (escolha == 2) {
 
-                for (Contato contato : bancoDeDados.buscaTodos()) {
-                    System.out.println("Nome: " + contato.getNome() + " - Fone: " + contato.getFone());
-                }
-
-            }
         } while (escolha != 0);
 
 
+    }
+
+    private static void listarTodos() {
+        for (Contato contato : bancoDeDados.buscaTodos()) {
+            System.out.println(contato.getId() + " - Nome: " + contato.getNome() + " - Fone: " + contato.getFone());
+        }
+    }
+
+    private static void cadastrar() {
+        String nome;
+        String fone;
+        System.out.print("Digite o nome do contato: ");
+        nome = entrada.next();
+        System.out.print("Digite o telefone do contato: ");
+        fone = entrada.next();
+        Contato contato = new Contato(nome, fone);
+        bancoDeDados.salva(contato);
     }
 
     private static void getMenu() {
